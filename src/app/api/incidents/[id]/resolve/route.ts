039a70624ab41b4262e91e1c5ff6324691fb9b88
Sourabh Goyal <sourabhgoyal560@gmail.com>
2025-07-25 17:29:56 +0530

    fixed patch request

--- src/app/api/incidents/[id]/resolve/route.ts
+++ src/app/api/incidents/[id]/resolve/route.ts
@@ -2,9 +2,10 @@ import { NextRequest, NextResponse } from 'next/server';
 import { prisma } from '../../../../../../lib/prisma';
 
 export async function PATCH(
-  request: NextRequest, id : number 
+  request: Request, {params} : {params : Promise<{id : number}>}
 ) {
 
+  const {id} = await params;
   const updated = await prisma.incident.update({
     where: { id: id },
     data: { resolved: true },
